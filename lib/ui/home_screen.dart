fcb1eb1cbd68f8adf3e2c9dd3c9ec335dc1d5b84
Sam Zaccone <samrzaccone@gmail.com>
2024-08-03 21:52:18 -0400

    add home feed

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vertex_virtual/navigation.dart';
 
+import '../features/articles/articles_controller.dart';
+import '../utility/error_loader.dart';
+
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
@@ -12,6 +15,37 @@ class HomeScreen extends ConsumerWidget {
         leading: IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
         actions: [TextButton(onPressed: () => navigateToCreateArticle(context), child: const Text('post'))],
       ),
+      body: ref.watch(articleFeedProvider).when(
+          data: (data) {
+            data.sort(
+              (a, b) => b.upvoteIds.length.compareTo(a.upvoteIds.length),
+            );
+            return ListView.builder(
+              itemCount: data.length + 1,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return const Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [Text('      title'), Text('score   ')],
+                      ),
+                      Divider(),
+                    ],
+                  );
+                }
+                final article = data[index - 1];
+                return ListTile(
+                  onTap: () {},
+                  title: Text(article.title),
+                  trailing: Text("${article.upvoteIds.length.toString()}  "),
+                );
+              },
+            );
+          },
+          error: (error, _) => ErrorText(error.toString()),
+          loading: () => const Loader()),
     );
   }
 }
